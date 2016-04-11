f64030a2ebf883b216c59207108c29ba29542ff5
Alin CHIS <alin.d.chis@gmail.com>
2016-04-11 11:44:51 +0300

    modified index.jl file

--- docker/escher/index.jl
+++ docker/escher/index.jl
@@ -1,22 +1,22 @@
-function main(window)
+import Escher: @api, render
+
+desc = md"""
+# Hello, World!
+You can write any **Markdown** text and place it in the middle of other
+tiles.
+""" |> Escher.pad(1em)
+
+main(window) = begin
+    push!(window.assets, "layout2")
+    push!(window.assets, "icons")
     push!(window.assets, "widgets")
 
-    inp = Signal(Dict())
+    t, p = wire(tabs([hbox(icon("home"), hskip(1em), "Home"),
+                      hbox(icon("info-outline"), hskip(1em),  "Notifications"),
+                      hbox(icon("settings"), hskip(1em), "Settings")]),
+                pages([desc, "Notification tab content", "Settings tab content"]), :tabschannel, :selected)
 
-    s = Escher.sampler()
-    form = vbox(
-        h1("Submit your rating"),
-        watch!(s, :name, textinput("", label="Your name")),
-        hbox("Your rating", watch!(s, :rating, slider(1:10)))
-            |> packacross(center),
-        trigger!(s, :submit, button("Submit"))
-    ) |> maxwidth(400px)
-
-    map(inp) do dict
-        vbox(
-            intent(s, form) >>> inp,
-            vskip(2em),
-            string(dict)
-        ) |> Escher.pad(2em)
-    end
+    vbox(toolbar([iconbutton("face"), "My App", flex(), iconbutton("search")]),
+         maxwidth(30em, t),
+         Escher.pad(1em, p))
 end
