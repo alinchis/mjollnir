43e67fd266384453d1c474da4315acf0d896eca5
Alin Chis <alin.d.chis@gmail.com>
2016-12-08 16:44:08 +0200

    first test - changed background

--- app3/index.jl
+++ app3/index.jl
@@ -9,8 +9,23 @@
 Base.convert(::Type{Container{:html}}, c::Cursor) = begin
   change(e) = put!(c, e)
   @dom [:html
-    [:head [:style :rel="stylesheet" :type="text/css" :href="./main.css"]]
-    [:body onclick=change
+    css"""
+    height: 100%
+    width:100%
+    margin: 0px
+    overflow: hidden
+    background-image: linear-gradient(to bottom, #58c 0%, #3162a3 100%)
+    """
+
+    [:head stylesheets...]
+    [:body css"""
+           display: flex
+           justify-content: space-around
+           align-items: center
+           background-image: linear-gradient(0deg, transparent 0%, transparent 9px, rgba(255, 255, 255, 0.2) 9px, rgba(255, 255, 255, 0.2) 10px, transparent 10px, transparent 19px, rgba(255, 255, 255, 0.1) 19px, rgba(255, 255, 255, 0.1) 20px, transparent 20px, transparent 29px, rgba(255, 255, 255, 0.1) 29px, rgba(255, 255, 255, 0.1) 30px, transparent 30px, transparent 39px, rgba(255, 255, 255, 0.1) 39px, rgba(255, 255, 255, 0.1) 40px, transparent 40px, transparent 49px, rgba(255, 255, 255, 0.1) 49px, rgba(255, 255, 255, 0.1) 50px), linear-gradient(-90deg, transparent 0%, transparent 9px, rgba(255, 255, 255, 0.2) 9px, rgba(255, 255, 255, 0.2) 10px, transparent 10px, transparent 19px, rgba(255, 255, 255, 0.1) 19px, rgba(255, 255, 255, 0.1) 20px, transparent 20px, transparent 29px, rgba(255, 255, 255, 0.1) 29px, rgba(255, 255, 255, 0.1) 30px, transparent 30px, transparent 39px, rgba(255, 255, 255, 0.1) 39px, rgba(255, 255, 255, 0.1) 40px, transparent 40px, transparent 49px, rgba(255, 255, 255, 0.1) 49px, rgba(255, 255, 255, 0.1) 50px)
+           background-size: 50px 50px
+           """
+           onclick=change
            onmousemove=change
            onkeydown=change
       [:pre repr(need(c))]]]
